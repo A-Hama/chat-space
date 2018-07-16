96b202dc55bd314a8aa9b9d00c08417bfc791ebb
浜野秋仁 <s.sakae3354@gmail.com>
2018-07-16 15:35:08 +0900

    Edit groups_controller, views, stylesheets, and Edit routes.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,18 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
     @group = Group.create(group_params)
     if @group.save
-      redirect_to 
+      redirect_to group_messages_path(@group), notice: 'グループを作成しました'
     else
       render :new
     end
@@ -16,9 +22,9 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.update(group_params)
-    if @group.save
-      redirect_to
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: '更新しました'
     else
       render :edit
     end
@@ -26,7 +32,11 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
