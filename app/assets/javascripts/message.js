8ea209bbda03cdcfd330824edc2aea807e403ded
浜野秋仁 <s.sakae3354@gmail.com>
2018-07-23 18:21:32 +0900

    Edit message.js and jbuilder to send a message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,14 +9,43 @@ $(function(){
         <p class="chat-message__time">${ message.date }</p>
       </div>
       <p class="chat-message__body">${ message.text }</p>
+      <p class="chat-message__image">
+        <img src="${ message.image }">
+      </p>
     </li>`
     return html;
     }
 
+    function imageHTML(message){
+      var html = `
+      <li class="chat-message">
+        <div class="chat-message__header clearfix">
+          <p class="chat-message__name">${ message.user_name }</p>
+          <p class="chat-message__time">${ message.date }</p>
+        </div>
+        <p class="chat-message__image">
+          <img src="${ message.image }">
+        </p>
+      </li>`
+      return html;
+    }
+
+    function textHTML(message){
+      var html = `
+      <li class="chat-message">
+        <div class="chat-message__header clearfix">
+          <p class="chat-message__name">${ message.user_name }</p>
+          <p class="chat-message__time">${ message.date }</p>
+        </div>
+        <p class="chat-message__body">${ message.text }</p>
+      </li>`
+      return html;
+    }
+
   $("#new_message").submit(function(e){
-    console.log("test");
     e.preventDefault();
     var formData = new FormData(this);
+    console.log(formData)
     var href = window.location.href;
     $.ajax({
       url: href,
@@ -27,15 +56,23 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      var html = buildHTML(data);
-      $('.chat-messages').append(html);
-      console.log(html)
-      $('.form__message').val('');
-      $(".form__submit").prop('disabled', false);
-      $('.chat-body').animate({scrollTop: $('.chat-body')[0].scrollHeight}, 'fast');
+      console.log(data)
+      if (data.text && data.image){
+        var html = buildHTML(data);
+      }else if(data.text && data.image === ''){
+        var html = textHTML(data);
+      }else if (data.text == '' && data.image){
+        var html = imageHTML(data);
+      }
+        $('.chat-messages').append(html);
+        $('.form__message').val('');
+        $(".form__submit").prop('disabled', false);
+        $('.chat-body').animate({scrollTop: $('.chat-body')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
-      alert('error');
+      alert('入力してください');
+      $(".form__submit").prop('disabled', false);
+      $('.form__message').val('');
     })
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,9 +1,5 @@
-if @message.body.present?
-  json.text  @message.body
-end
-if @message.image.url.present?
-  json.image @message.image.url
-end
+json.text  @message.body
+json.image @message.image.url
 json.user_id  @message.user.id
 json.user_name  @message.user.name
 json.group_id @message.group.id
