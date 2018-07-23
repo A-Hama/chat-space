2445d5e09e457aea930c052a42ddd89f7c187406
浜野秋仁 <s.sakae3354@gmail.com>
2018-07-23 19:44:28 +0900

    Edit indents in message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,18 +2,18 @@ $(function(){
   function buildHTML(message){
     var image = message.image ? `<img src=${ message.image }>` : " ";
     var html = `
-    <li class="chat-message">
-      <div class="chat-message__header clearfix">
-        <p class="chat-message__name">${ message.user_name }</p>
-        <p class="chat-message__time">${ message.date }</p>
-      </div>
-      <p class="chat-message__body">${ message.text }</p>
-      <p class="chat-message__image">
-          ${ image }
-      </p>
-    </li>`
+      <li class="chat-message">
+        <div class="chat-message__header clearfix">
+          <p class="chat-message__name">${ message.user_name }</p>
+          <p class="chat-message__time">${ message.date }</p>
+        </div>
+        <p class="chat-message__body">${ message.text }</p>
+        <p class="chat-message__image">
+            ${ image }
+        </p>
+      </li>`
     return html;
-    }
+  }
 
   $("#new_message").submit(function(e){
     e.preventDefault();
