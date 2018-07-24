9e20d091dcc232b4b32538b5b72c65d221e57624
浜野秋仁 <s.sakae3354@gmail.com>
2018-07-24 21:35:30 +0900

    Edit message.js (var url = $(this).attr('action');)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,7 +22,7 @@ $(function(){
   $("#new_message").submit(function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action'),
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
