4a3ffaadd1957863440ec5e55d71228e60a01d22
浜野秋仁 <s.sakae3354@gmail.com>
2018-08-03 19:20:53 +0900

    include logger

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,6 +12,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+    logger.debug(@message.save)
     if @message.save
       respond_to do |format|
         format.html{ redirect_to group_messages_path(@group) , notice: 'メッセージが送信されました'}
